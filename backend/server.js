10c2e8216ee6b5b8b763f140071189c5499b4457
Inesbourguibaweb <inesbourguibaweb@gmail.com>
2024-05-20 22:00:50 +0100

    update package.json

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import products from './data/products.js';
 const port = 5000;
 
 const app = express();
@@ -6,5 +7,12 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('API is running');
 });
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((product) => product._id === req.params.id);
+  res.json(product);
+});
 
-app.listen(port, () => console.log(`server is running on the server${port}`));
+app.listen(port, () => console.log(`server is running on the server ${port}`));
